99f0c1abedf1a81b39318f92f06e84d5850658d8
Federico Rodriguez <federico.rodriguez@wazuh.com>
2026-02-16 20:36:04 +0100

    Format

--- public/pages/Integrations/components/PolicyInfo.tsx
+++ public/pages/Integrations/components/PolicyInfo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withGuardAsync } from '../utils/helpers';
 import { DataStore } from '../../../store/DataStore';
-import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiSpacer, EuiText, EuiDescriptionListTitle, EuiDescriptionListDescription, EuiDescriptionList } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiText, EuiDescriptionListTitle, EuiDescriptionListDescription, EuiDescriptionList } from '@elastic/eui';
 import { DecoderSource, PolicyDocument, Space } from '../../../../types';
 import { ButtonSelectRootDecoder } from './RootDecoderRequirement';
 import { NotificationsStart } from 'opensearch-dashboards/public';
@@ -50,24 +50,24 @@ export const PolicyInfoCard: React.FC<{}> = withPolicyGuard(
   }) => {
     return (
       <EuiFlexGroup>
-    <EuiFlexItem>
-        <EuiDescriptionList compressed type="row">
-          <EuiDescriptionListTitle>Title</EuiDescriptionListTitle>
-          <EuiDescriptionListDescription>{policyDocumentData.title}</EuiDescriptionListDescription>
-          <EuiDescriptionListTitle>Description</EuiDescriptionListTitle>
-          <EuiDescriptionListDescription>{policyDocumentData.description}</EuiDescriptionListDescription>
-          <EuiDescriptionListTitle>Author</EuiDescriptionListTitle>
-          <EuiDescriptionListDescription>{policyDocumentData.author}</EuiDescriptionListDescription>
-        </EuiDescriptionList>
-    </EuiFlexItem>
-    <EuiFlexItem>
-        <EuiDescriptionList compressed type="row">
-          <EuiDescriptionListTitle>Documentation</EuiDescriptionListTitle>
-          <EuiDescriptionListDescription>{policyDocumentData.documentation}</EuiDescriptionListDescription>
-          <EuiDescriptionListTitle>References</EuiDescriptionListTitle>
-          <EuiDescriptionListDescription>{policyDocumentData.references.join(', ')}</EuiDescriptionListDescription>
-          <EuiDescriptionListTitle>Root decoder</EuiDescriptionListTitle>
-          <EuiDescriptionListDescription>
+        <EuiFlexItem>
+          <EuiDescriptionList compressed type="row">
+            <EuiDescriptionListTitle>Title</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>{policyDocumentData.title}</EuiDescriptionListDescription>
+            <EuiDescriptionListTitle>Description</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>{policyDocumentData.description}</EuiDescriptionListDescription>
+            <EuiDescriptionListTitle>Author</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>{policyDocumentData.author}</EuiDescriptionListDescription>
+          </EuiDescriptionList>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiDescriptionList compressed type="row">
+            <EuiDescriptionListTitle>Documentation</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>{policyDocumentData.documentation}</EuiDescriptionListDescription>
+            <EuiDescriptionListTitle>References</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>{policyDocumentData.references.join(', ')}</EuiDescriptionListDescription>
+            <EuiDescriptionListTitle>Root decoder</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>
               {rootDecoder?.document?.name ?? ''}
               {actionIsAllowedOnSpace(space, SPACE_ACTIONS.DEFINE_ROOT_DECODER) && (
                 <ButtonSelectRootDecoder
@@ -80,10 +80,10 @@ export const PolicyInfoCard: React.FC<{}> = withPolicyGuard(
                   onConfirm={check}
                 ></ButtonSelectRootDecoder>
               )}
-          </EuiDescriptionListDescription>
-        </EuiDescriptionList>
-      </EuiFlexItem>
-    </EuiFlexGroup>
+            </EuiDescriptionListDescription>
+          </EuiDescriptionList>
+        </EuiFlexItem>
+      </EuiFlexGroup>
     );
   }
 );
