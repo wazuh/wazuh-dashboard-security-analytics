7172e6d0ca3e8c81169f31d6aea349d84d194a9b
Federico Rodriguez <federico.rodriguez@wazuh.com>
2026-02-17 13:44:17 +0100

    Render the integration details outside the table card (#72)
    Co-authored-by: Antonio <34042064+Desvelao@users.noreply.github.com>
    Co-authored-by: Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>

--- public/pages/Integrations/components/PolicyInfo.tsx
+++ public/pages/Integrations/components/PolicyInfo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withGuardAsync } from '../utils/helpers';
 import { DataStore } from '../../../store/DataStore';
-import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiSpacer, EuiText, EuiTitle } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiText, EuiDescriptionListTitle, EuiDescriptionListDescription, EuiDescriptionList } from '@elastic/eui';
 import { DecoderSource, PolicyDocument, Space } from '../../../../types';
 import { ButtonSelectRootDecoder } from './RootDecoderRequirement';
 import { NotificationsStart } from 'opensearch-dashboards/public';
@@ -49,22 +49,26 @@ export const PolicyInfoCard: React.FC<{}> = withPolicyGuard(
     check;
   }) => {
     return (
-      <EuiPanel paddingSize="s">
-        <EuiTitle size="s">
-          <h3>Space info</h3>
-        </EuiTitle>
-        <EuiSpacer size="s" />
-        <EuiFlexGroup>
-          <EuiFlexItem>Title: {policyDocumentData.title}</EuiFlexItem>
-          <EuiFlexItem>Description: {policyDocumentData.description}</EuiFlexItem>
-          <EuiFlexItem>Author: {policyDocumentData.author}</EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiFlexGroup alignItems="center">
-          <EuiFlexItem>Documentation: {policyDocumentData.documentation}</EuiFlexItem>
-          <EuiFlexItem>References: {policyDocumentData.references.join(', ')}</EuiFlexItem>
-          <EuiFlexItem>
-            <div style={{ display: 'flex', alignItems: 'center', flexGrow: 0 }}>
-              <div style={{ flexGrow: 0 }}>Root decoder: {rootDecoder?.document?.name ?? ''}</div>
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          <EuiDescriptionList compressed type="row">
+            <EuiDescriptionListTitle>Title</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>{policyDocumentData.title}</EuiDescriptionListDescription>
+            <EuiDescriptionListTitle>Description</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>{policyDocumentData.description}</EuiDescriptionListDescription>
+            <EuiDescriptionListTitle>Author</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>{policyDocumentData.author}</EuiDescriptionListDescription>
+          </EuiDescriptionList>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiDescriptionList compressed type="row">
+            <EuiDescriptionListTitle>Documentation</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>{policyDocumentData.documentation}</EuiDescriptionListDescription>
+            <EuiDescriptionListTitle>References</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>{policyDocumentData.references.join(', ')}</EuiDescriptionListDescription>
+            <EuiDescriptionListTitle>Root decoder</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>
+              {rootDecoder?.document?.name ?? ''}
               {actionIsAllowedOnSpace(space, SPACE_ACTIONS.DEFINE_ROOT_DECODER) && (
                 <ButtonSelectRootDecoder
                   notifications={notifications}
@@ -76,10 +80,10 @@ export const PolicyInfoCard: React.FC<{}> = withPolicyGuard(
                   onConfirm={check}
                 ></ButtonSelectRootDecoder>
               )}
-            </div>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiPanel>
+            </EuiDescriptionListDescription>
+          </EuiDescriptionList>
+        </EuiFlexItem>
+      </EuiFlexGroup>
     );
   }
 );
