c5c26f5089ce24346fc0a17bba6f270413263da5
Diego García <82405377+Ripdiegozz@users.noreply.github.com>
2026-02-20 11:58:51 -0500

    Detection rules wording changes (#76)
    Co-authored-by: Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>

--- public/pages/Integrations/components/IntegrationDetectionRules.tsx
+++ public/pages/Integrations/components/IntegrationDetectionRules.tsx
@@ -37,7 +37,7 @@ export const IntegrationDetectionRules: React.FC<IntegrationDetectionRulesProps>
     <>
       {flyoutData && <RuleViewerFlyout hideFlyout={hideFlyout} ruleTableItem={flyoutData} />}
       <ContentPanel
-        title="Detection rules"
+        title="Rules"
         hideHeaderBorder={true}
         actions={[<EuiSmallButton onClick={refreshRules}>Refresh</EuiSmallButton>]}
       >
@@ -46,7 +46,7 @@ export const IntegrationDetectionRules: React.FC<IntegrationDetectionRulesProps>
             <EuiFlexItem grow={false}>
               <EuiText color="subdued" size="s">
                 {/* By Wazuh */}
-                <p>There are no detection rules associated with this integration. </p>
+                <p>There are no rules associated with this integration. </p>
               </EuiText>
             </EuiFlexItem>
             <EuiFlexItem grow={false}>
@@ -55,7 +55,7 @@ export const IntegrationDetectionRules: React.FC<IntegrationDetectionRulesProps>
                 href={`opensearch_security_analytics_dashboards#/create-rule`}
                 target="_blank"
               >
-                Create detection rule&nbsp;
+                Create rule&nbsp;
                 <EuiIcon type={'popout'} />
               </EuiSmallButton>
               <EuiSpacer size="xl" />
