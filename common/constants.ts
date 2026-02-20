4a4939bcfe671a5c09a1be3148eb63a3104cb9d2
Fernando Castilla Ropero <fernando.castilla@wazuh.com>
2026-02-20 11:09:53 +0100

    Fix format

--- common/constants.ts
+++ common/constants.ts
@@ -3,52 +3,50 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { wazuh } from "../package.json";
+import { wazuh } from '../package.json';
 
-export const DEFAULT_RULE_UUID = "25b9c01c-350d-4b95-bed1-836d04a4f324";
+export const DEFAULT_RULE_UUID = '25b9c01c-350d-4b95-bed1-836d04a4f324';
 
 export const WAZUH_VERSION = wazuh.version;
 
-export const PLUGIN_VERSION_SHORT = WAZUH_VERSION.split(".")
-  .splice(0, 2)
-  .join(".");
+export const PLUGIN_VERSION_SHORT = WAZUH_VERSION.split('.').splice(0, 2).join('.');
 
 export enum ThreatIntelIocSourceType {
-  S3_CUSTOM = "S3_CUSTOM",
-  IOC_UPLOAD = "IOC_UPLOAD",
-  URL_DOWNLOAD = "URL_DOWNLOAD",
+  S3_CUSTOM = 'S3_CUSTOM',
+  IOC_UPLOAD = 'IOC_UPLOAD',
+  URL_DOWNLOAD = 'URL_DOWNLOAD',
 }
 
 export const SpaceTypes = {
   DRAFT: {
-    label: "Draft",
-    value: "draft",
+    label: 'Draft',
+    value: 'draft',
     description:
-      "Staging area for creating or editing resources before testing.",
+      'Staging area for creating or editing resources before testing.',
   },
   TEST: {
-    label: "Test",
-    value: "test",
-    description: "Controlled environment for validation before production.",
+    label: 'Test',
+    value: 'test',
+    description: 'Controlled environment for validation before production.',
   },
   CUSTOM: {
-    label: "Custom",
-    value: "custom",
-    description: "Independent space for custom or modified content.",
+    label: 'Custom',
+    value: 'custom',
+    description: 'Independent space for custom or modified content.',
   },
   STANDARD: {
-    label: "Standard",
-    value: "standard",
-    description: "Wazuh CTI provided resources.",
+    label: 'Standard',
+    value: 'standard',
+    description: 'Wazuh CTI provided resources.',
   },
 } as const;
 
 export const SPACE_ACTIONS = {
-  CREATE: "create",
-  EDIT: "edit",
-  DELETE: "delete",
-  PROMOTE: "promote",
-  DEFINE_ROOT_DECODER: "define_root_decoder",
+  CREATE: 'create',
+  EDIT: 'edit',
+  DELETE: 'delete',
+  PROMOTE: 'promote',
+  DEFINE_ROOT_DECODER: 'define_root_decoder',
 };
 
 export const AllowedActionsBySpace = {
