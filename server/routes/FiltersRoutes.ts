fcfb5722f353b4a3fdf7970503608c5dbf6cb8a8
Rodrigo Lopez <rodrigo.lopez@wazuh.com>
2026-03-10 16:41:53 +0000

    feat: add filters routes

--- server/routes/FiltersRoutes.ts
+++ server/routes/FiltersRoutes.ts
@@ -9,6 +9,25 @@ import { NodeServices } from '../models/interfaces';
 import { API } from '../utils/constants';
 import { createQueryValidationSchema } from '../utils/helpers';
 
+const filterResourceSchema = schema.object({
+  name: schema.string(),
+  enabled: schema.maybe(schema.boolean()),
+  check: schema.maybe(schema.string()),
+  type: schema.maybe(schema.string()),
+  metadata: schema.maybe(
+    schema.object({
+      description: schema.maybe(schema.string()),
+      author: schema.maybe(
+        schema.object({
+          name: schema.maybe(schema.string()),
+          email: schema.maybe(schema.string()),
+          url: schema.maybe(schema.string()),
+        })
+      ),
+    })
+  ),
+});
+
 export function setupFiltersRoutes(services: NodeServices, router: IRouter) {
   const { filtersService } = services;
 
@@ -21,5 +40,45 @@ export function setupFiltersRoutes(services: NodeServices, router: IRouter) {
       },
     },
     filtersService.searchFilters
+  );
+
+  router.post(
+    {
+      path: `${API.FILTERS_BASE}`,
+      validate: {
+        body: schema.object({
+          space: schema.string(),
+          resource: filterResourceSchema,
+        }),
+        query: createQueryValidationSchema(),
+      },
+    },
+    filtersService.createFilter
+  );
+
+  router.put(
+    {
+      path: `${API.FILTERS_BASE}/{filterId}`,
+      validate: {
+        params: schema.object({ filterId: schema.string() }),
+        body: schema.object({
+          space: schema.string(),
+          resource: filterResourceSchema,
+        }),
+        query: createQueryValidationSchema(),
+      },
+    },
+    filtersService.updateFilter
+  );
+
+  router.delete(
+    {
+      path: `${API.FILTERS_BASE}/{filterId}`,
+      validate: {
+        params: schema.object({ filterId: schema.string() }),
+        query: createQueryValidationSchema(),
+      },
+    },
+    filtersService.deleteFilter
   );
 }
