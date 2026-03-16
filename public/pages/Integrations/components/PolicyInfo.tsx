1baf28753add7c68cd10228ee3034337092166ab
Felipe Gonzalez <felipegonzalezmv@hotmail.com>
2026-03-16 15:05:57 -0500

    Normalize policy metadata for policies
    Nest policy title and other metadata under document.metadata to align with ruleset normalization (issue #117).

--- public/pages/Integrations/components/PolicyInfo.tsx
+++ public/pages/Integrations/components/PolicyInfo.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { withGuardAsync } from '../utils/helpers';
-import { DataStore } from '../../../store/DataStore';
+import React from "react";
+import { withGuardAsync } from "../utils/helpers";
+import { DataStore } from "../../../store/DataStore";
 import {
   EuiFlexGroup,
   EuiFlexItem,
@@ -8,26 +8,39 @@ import {
   EuiDescriptionListTitle,
   EuiDescriptionListDescription,
   EuiDescriptionList,
-} from '@elastic/eui';
-import { DecoderSource, PolicyDocument, SearchPolicyOptions, Space } from '../../../../types';
-import { ButtonSelectRootDecoder } from './RootDecoderRequirement';
-import { NotificationsStart } from 'opensearch-dashboards/public';
-import { SPACE_ACTIONS } from '../../../../common/constants';
-import { actionIsAllowedOnSpace } from '../../../../common/helpers';
+} from "@elastic/eui";
+import {
+  DecoderSource,
+  PolicyDocument,
+  SearchPolicyOptions,
+  Space,
+} from "../../../../types";
+import { ButtonSelectRootDecoder } from "./RootDecoderRequirement";
+import { NotificationsStart } from "opensearch-dashboards/public";
+import { SPACE_ACTIONS } from "../../../../common/constants";
+import { actionIsAllowedOnSpace } from "../../../../common/helpers";
 
 export const withPolicyGuard: <T>(
-  searchPolicyOptions: SearchPolicyOptions
+  searchPolicyOptions: SearchPolicyOptions,
 ) => (Component: React.FC<T>) => React.ReactElement = (
-  searchPolicyOptions: SearchPolicyOptions = {}
+  searchPolicyOptions: SearchPolicyOptions = {},
 ) =>
   withGuardAsync(
     async ({ space }: { space: Space }) => {
       try {
-        const response = await DataStore.policies.searchPolicies(space, searchPolicyOptions);
+        const response = await DataStore.policies.searchPolicies(
+          space,
+          searchPolicyOptions,
+        );
 
         const item = response.items?.[0] || {};
 
-        const { document: policyDocumentData, space: spaceData, id, ...rest } = item as {
+        const {
+          document: policyDocumentData,
+          space: spaceData,
+          id,
+          ...rest
+        } = item as {
           document?: PolicyDocument;
           [key: string]: any;
         };
@@ -35,7 +48,10 @@ export const withPolicyGuard: <T>(
         const rootDecoderId = policyDocumentData?.root_decoder;
         let rootDecoder: DecoderSource | undefined;
         if (rootDecoderId) {
-          rootDecoder = await DataStore.decoders.getDecoder(rootDecoderId, space); // TODO: this could be obtained from the endpoint as rest
+          rootDecoder = await DataStore.decoders.getDecoder(
+            rootDecoderId,
+            space,
+          ); // TODO: this could be obtained from the endpoint as rest
         }
 
         return {
@@ -47,75 +63,84 @@ export const withPolicyGuard: <T>(
       }
     },
     ({ error }) =>
-      error ? <EuiText color="danger">Error loading the policy: {error.message}</EuiText> : null,
+      error ? (
+        <EuiText color="danger">
+          Error loading the policy: {error.message}
+        </EuiText>
+      ) : null,
     null,
     {
       rerunOn: ({ space }) => [space],
-    }
+    },
   );
 
 export const PolicyInfoCard: React.FC<{}> = withPolicyGuard({
-  includeIntegrationFields: ['document'],
-})(
-  ({
-    policyDocumentData,
-    rootDecoder,
-    notifications,
-    space,
-    check,
-  }: {
-    policyDocumentData: PolicyDocument;
-    rootDecoder: DecoderSource;
-    notifications: NotificationsStart;
-    space: Space;
-    check;
-  }) => {
-    return (
-      <EuiFlexGroup>
-        <EuiFlexItem>
-          <EuiDescriptionList compressed type="row">
-            <EuiDescriptionListTitle>Title</EuiDescriptionListTitle>
-            <EuiDescriptionListDescription>
-              {policyDocumentData.title}
-            </EuiDescriptionListDescription>
-            <EuiDescriptionListTitle>Description</EuiDescriptionListTitle>
-            <EuiDescriptionListDescription>
-              {policyDocumentData.description}
-            </EuiDescriptionListDescription>
-            <EuiDescriptionListTitle>Author</EuiDescriptionListTitle>
-            <EuiDescriptionListDescription>
-              {policyDocumentData.author}
-            </EuiDescriptionListDescription>
-          </EuiDescriptionList>
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiDescriptionList compressed type="row">
-            <EuiDescriptionListTitle>Documentation</EuiDescriptionListTitle>
-            <EuiDescriptionListDescription>
-              {policyDocumentData.documentation}
-            </EuiDescriptionListDescription>
-            <EuiDescriptionListTitle>References</EuiDescriptionListTitle>
-            <EuiDescriptionListDescription>
-              {policyDocumentData.references.join(', ')}
-            </EuiDescriptionListDescription>
-            <EuiDescriptionListTitle>Root decoder</EuiDescriptionListTitle>
-            <EuiDescriptionListDescription>
-              {rootDecoder?.document?.name ?? ''}
-              {actionIsAllowedOnSpace(space, SPACE_ACTIONS.DEFINE_ROOT_DECODER) && (
-                <ButtonSelectRootDecoder
-                  notifications={notifications}
-                  space={space}
-                  type="icon"
-                  buttonProps={{ iconType: 'pencil', 'aria-label': 'Edit root decoder' }}
-                  policyDocumentData={policyDocumentData}
-                  rootDecoderSource={rootDecoder}
-                  onConfirm={check}
-                ></ButtonSelectRootDecoder>
-              )}
-            </EuiDescriptionListDescription>
-          </EuiDescriptionList>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    );
-  }
-);
+  includeIntegrationFields: ["document"],
+})(({
+  policyDocumentData,
+  rootDecoder,
+  notifications,
+  space,
+  check,
+}: {
+  policyDocumentData: PolicyDocument;
+  rootDecoder: DecoderSource;
+  notifications: NotificationsStart;
+  space: Space;
+  check;
+}) => {
+  const metadata = policyDocumentData.metadata;
+  return (
+    <EuiFlexGroup>
+      <EuiFlexItem>
+        <EuiDescriptionList compressed type="row">
+          <EuiDescriptionListTitle>Title</EuiDescriptionListTitle>
+          <EuiDescriptionListDescription>
+            {metadata?.title}
+          </EuiDescriptionListDescription>
+          <EuiDescriptionListTitle>Description</EuiDescriptionListTitle>
+          <EuiDescriptionListDescription>
+            {metadata?.description}
+          </EuiDescriptionListDescription>
+          <EuiDescriptionListTitle>Author</EuiDescriptionListTitle>
+          <EuiDescriptionListDescription>
+            {metadata?.author}
+          </EuiDescriptionListDescription>
+        </EuiDescriptionList>
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiDescriptionList compressed type="row">
+          <EuiDescriptionListTitle>Documentation</EuiDescriptionListTitle>
+          <EuiDescriptionListDescription>
+            {metadata?.documentation}
+          </EuiDescriptionListDescription>
+          <EuiDescriptionListTitle>References</EuiDescriptionListTitle>
+          <EuiDescriptionListDescription>
+            {metadata?.references?.join(", ")}
+          </EuiDescriptionListDescription>
+          <EuiDescriptionListTitle>Root decoder</EuiDescriptionListTitle>
+          <EuiDescriptionListDescription>
+            {rootDecoder?.document?.name ?? ""}
+            {actionIsAllowedOnSpace(
+              space,
+              SPACE_ACTIONS.DEFINE_ROOT_DECODER,
+            ) && (
+              <ButtonSelectRootDecoder
+                notifications={notifications}
+                space={space}
+                type="icon"
+                buttonProps={{
+                  iconType: "pencil",
+                  "aria-label": "Edit root decoder",
+                }}
+                policyDocumentData={policyDocumentData}
+                rootDecoderSource={rootDecoder}
+                onConfirm={check}
+              ></ButtonSelectRootDecoder>
+            )}
+          </EuiDescriptionListDescription>
+        </EuiDescriptionList>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+});
