04b7a2256b68d9243df8805da2840d3ef54ab333
Adam Navarro Megías <adam.navarro@wazuh.com>
2026-07-08 10:46:19 +0200

    restore style changes
    Signed-off-by: Adam Navarro Megías <adam.navarro@wazuh.com>

--- public/store/IntegrationStore.ts
+++ public/store/IntegrationStore.ts
@@ -22,10 +22,7 @@ import {
 } from '../utils/constants';
 
 export class IntegrationStore {
-  constructor(
-    private service: IntegrationService,
-    private notifications: NotificationsStart
-  ) {}
+  constructor(private service: IntegrationService, private notifications: NotificationsStart) {}
 
   private formatRelatedEntitiesList(entities: string[]): string {
     if (entities.length === 0) {
